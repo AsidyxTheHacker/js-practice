39966a6ac5af5e8bb8d8974ace71d67a03aafb8b
Adam <adammiller.2000nj@gmail.com>
2022-10-17 01:06:08 -0400

    added js comments

--- javascript.js
+++ javascript.js
@@ -1,3 +1,27 @@
-let message;
+let message = "Hello World!"; // define the variable and assign the value
+let user = 'John';
+let age = 25;
+alert(message); // variable 'message' pops up
 
-message = 'Hello' //store the string 'Hello' in the variable named message
+// let: defines a variable.
+// const: defines an unchangable variable.
+
+const birthday = '01/09/2000';
+// birthday = '12/31/2022';  (error, can't reassign the constant!)
+
+// let one = 3;
+// let two = 6;
+// alert(one + two); (alert product of variables(1,2))
+
+// let user = "John", age = 25, message = "Hello"; 
+// (ability to set values and variables within 1 line)
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+//alert(color); // #FF7F00
+
+document.write("this is funny text lol")
