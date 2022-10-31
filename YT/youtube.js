5010b22e82df3591158e9eed3ba7edcd1be6fe27
Adam <adammiller.2000nj@gmail.com>
2022-10-30 22:47:33 -0400

    added regex practice

--- YT/youtube.js
+++ YT/youtube.js
@@ -15,3 +15,11 @@ console.log("Enrolled:", student);
 document.getElementById("p1").innerHTML = "Hello " + firstName;
 document.getElementById("p2").innerHTML = "You are " + age + " years old";
 document.getElementById("p3").innerHTML = "Enrolled: " + student;
+
+// arithmetic expressions
+// - operands (variables, valuesm, etc.)
+// - operators (+ - * / % **)
+
+let students = 20;
+students = students + 1; // students += 1;
+console.log(students);
